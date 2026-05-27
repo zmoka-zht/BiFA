81183132676b092325901965c74b194c8591247d
Zhang haotian <60415344+zmoka-zht@users.noreply.github.com>
2026-05-27 13:16:35 +0800

    fix bug
    Comment out the call to get_scheduler during training.

--- train_cd.py
+++ train_cd.py
@@ -293,7 +293,7 @@ if __name__ == '__main__':
 
                     metric.clear()
 
-            get_scheduler(optimizer=optimer, args=opt['train']).step()
+            # get_scheduler(optimizer=optimer, args=opt['train']).step()
         logger.info('End of training.')
 
     else:
